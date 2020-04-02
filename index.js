e014b3dff17f8914b8d4ab822ebdd0c7e678d031
Rizal Adoy <rizal.adoy@gmail.com>
2020-04-02 07:46:49 +0700

    add db feed

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
 require('dotenv').config();
 
+// Slackbot
 const SlackBot = require('slackbots');
 const bot = new SlackBot({
     token: process.env.slackbot_token,
     name: 'GenrowBot'
 });
 
-bot.on('start', function () {
-    let params = {
-        icon_emoji: ':cat:'
-    };
-
-    bot.postMessageToChannel('general', 'meow!', params);
-    bot.postMessageToUser('user_name', 'meow!', params);
-    bot.postMessageToUser('user_name', 'meow!', {'slackbot': true, icon_emoji: ':cat:'});
-    bot.postMessageToGroup('private_group', 'meow!', params);
-
-
+// RSS Feed
+const FeedSub = require('feedsub');
+const reader = new FeedSub(process.env.rss_feed, {
+    interval: 1
 });
+
+// Low DB
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+// Create db
+db.defaults({feed: []}).write();
+
+bot.on('start', function () {
+    bot.postMessageToChannel('general', 'please stay at home!');
+});
+
+bot.on("message", msg => {
+    if (msg.type === "message") {
+        if (msg.channel[0] === "D" && msg.bot_id === undefined) {
+            bot.postMessage(msg.user, "hi", {as_user: true});
+
+            let item = db.get('feed')
+                .filter({published: true})
+                .sortBy('views')
+                .take(5)
+                .value();
+
+            for (let i of item) {
+                bot.postMessage(msg.user, i.title, {as_user: true});
+            }
+        }
+    }
+});
+
+reader.on('item', (item) => {
+    console.log(item.title);
+
+    const itemInDb = db.get('feed').find({link: item.link}).value();
+    if (!itemInDb) {
+        if (item.title !== 'No title') {
+            db.get('feed').push(item).write();
+        }
+    }
+});
+
+reader.start();
