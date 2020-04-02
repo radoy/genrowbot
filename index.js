4acdb0ba72b17faf552172c03ef522638f3d77f0
Rizal Adoy <rizal.adoy@gmail.com>
2020-04-02 11:13:35 +0700

    fix crashed

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ const adapter = new FileSync('db.json');
 const db = low(adapter);
 
 // Create db
-db.defaults({feed: []}).write();
-
-console.log(process.env.rss_feed);
+// db.defaults({feed: []}).write();
 
 bot.on('start', function () {
     bot.postMessageToChannel('general', 'please stay at home!');
